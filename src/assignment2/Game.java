5560bf81b233650cca6bfcee1b54daa37215fdbd
dannyv7 <dannyphvo.com>
2016-06-15 14:23:49 +0000

    First attempts

--- src/assignment2/Game.java
+++ src/assignment2/Game.java
@@ -1,5 +1,39 @@
 package assignment2;
 
-public class Game {
+import java.lang.StringBuilder;
+import java.util.Random;
+import java.util.Scanner;
 
+/**
+ * This class controls the general game state
+ * @author danny
+ *
+ */
+public class Game {
+	private static String colors = "RYBG";
+	private static String code = generateCode(colors, 4);
+	private static boolean activeGame = true;
+	
+	public static void main(String args[]){
+		while(activeGame){
+			System.out.println("Enter your guess: ");
+			Scanner guess = new Scanner(System.in);
+			PlayerIn temp = new PlayerIn(guess.nextLine(), code);
+		}
+	}
+	
+	/**
+	 * Generates the code to be guessed by the player
+	 * @param colorPool The  colors that can be chosen from
+	 * @param len Length of the code
+	 * @return Randomly generated code in String format
+	 */
+	public static String generateCode(String colorPool, int len){
+		StringBuilder temp = new StringBuilder();
+		Random rand = new Random();
+		for(int i = 0; i <len; i += 1){
+			temp.append(colorPool.charAt(rand.nextInt(colorPool.length())));
+		}
+		return temp.toString();
+	}
 }
--- src/assignment2/PlayerIn.java
+++ src/assignment2/PlayerIn.java
@@ -1,5 +1,26 @@
 package assignment2;
 
 public class PlayerIn {
-
+	String aiCode;
+	String playerGuess;
+	
+	/**
+	 * Constructor for PlayerIn class
+	 * @param input User input when prompted
+	 * @param code Colors selected by the AI
+	 */
+	public PlayerIn(String input, String code){
+		aiCode = code;
+		playerGuess = input;
+	}
+	
+	/**
+	 * Checks to see if the user input is valid
+	 * @return True or False depending on user input format
+	 */
+	public boolean isFormatCorrect(){
+		if(playerGuess.length() != aiCode.length()){
+			return false;
+		}else{ return true; }
+	}
 }
