47ef9ffc4c1876a045a91db8c083ae7f218ef7f6
dannyv7 <dannyphvo.com>
2016-06-16 23:21:56 +0000

    Added a prompt that checks for the player's ready status

--- src/assignment2/Mastermind.java
+++ src/assignment2/Mastermind.java
@@ -1,8 +1,20 @@
 package assignment2;
 
+import java.util.Scanner;
+
 public class Mastermind {
 
 	public static void main(String[] args) {
+		boolean launch = true;
+		while(launch){
+			System.out.println("Are you ready to play? (Y/N) :");
+			Scanner ready = new Scanner(System.in);
+			String cmp = new String("Y");
+			String cmp1 = ready.nextLine();
+			if(cmp.equals(cmp1)){
+				launch = false;
+			}
+		}
 		Game x = new Game();
 		x.runGame();
 
