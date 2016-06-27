9f26c8050170da4da1d8c6687b999e24fe217feb
Joel Guo <jguo48@gmail.com>
2016-06-27 18:55:12 -0500

    fixed history not sure about UML format added header

---
+++ src/assignment2/Header.java
@@ -0,0 +1,10 @@
+/*  * EE422C Project 2 (Mastermind) submission by
+ * Replace <...> with your actual data. 
+ * <Student 1 Name>
+ * <Student 1 EID>
+ * <Student 2 Name>
+ * <Student 2 EID>
+ * <Your GIT URL>
+ * Slip days used: <0>
+ * Summer 2016
+ */
